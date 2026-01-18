5d8bb0f525396c501f8c2863a0901534ecc49c35
mdedealf <dede.alfaruqq@gmail.com>
2026-01-18 17:37:54 +0700

    feat: adding get all patients api

---
+++ patient-service/src/main/java/com/mdedealf/patientservice/repository/PatientRepository.java
@@ -0,0 +1,12 @@
+package com.mdedealf.patientservice.repository;
+
+import com.mdedealf.patientservice.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, UUID> {
+
+}
